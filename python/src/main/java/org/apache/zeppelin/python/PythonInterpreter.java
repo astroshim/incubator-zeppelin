a28ab7d7d77401efb56af191010dbdc6126077af
astroshim <hsshim@zepl.com>
2017-01-20 01:39:37 -0800

    testing..

--- python/src/main/java/org/apache/zeppelin/python/PythonInterpreter.java
+++ python/src/main/java/org/apache/zeppelin/python/PythonInterpreter.java
@@ -271,6 +271,14 @@ public class PythonInterpreter extends Interpreter implements ExecuteResultHandl
       return new InterpreterResult(Code.SUCCESS, "");
     }
 
+    /*
+    try {
+      Thread.sleep(5000);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+    */
+
     this.context = contextInterpreter;
 
     if (!pythonscriptRunning) {
